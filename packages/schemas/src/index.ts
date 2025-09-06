0854b660ab57def0050821e39bb3a58badc32756
Shriyansh Lohia <whoshriyansh@gmail.com>
2025-09-06 12:06:42 +0530

    Intalled Zod in the share package and it's working and also installed tailwind css and added dark and light mode

---
+++ packages/schemas/src/index.ts
@@ -0,0 +1,10 @@
+// packages/schemas/src/index.ts
+import { z } from "zod";
+
+export const UserSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  email: z.string().email(),
+});
+
+export type User = z.infer<typeof UserSchema>;
